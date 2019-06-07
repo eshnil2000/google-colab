204ea8c4852cf6ecc086a89c422e49c882a7df81
Nick <eshnil2000@gmail.com>
2019-06-07 10:52:11 -0700

    Update test_nn.py

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import TestCase
-
+from ... import dutils
 from dutils.neural_network import NeuralNetwork
 from dutils.dataset import SampleCircleDataset, SampleSwirlDataset,\
 	SampleHalfDividedDataset, SampleAcrossCornerDataset
